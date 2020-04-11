deb8d5b4b7a17e12c497f499ace3d93de280a5f3
Wender Machado <wender.machado@rgsistemas.com.br>
2020-04-11 13:49:21 -0300

    :lipstick: Updated global style

--- web/src/styles/global.ts
+++ web/src/styles/global.ts
@@ -4,12 +4,19 @@ export default createGlobalStyle`
   * {
     margin: 0;
     padding: 0;
+    outline: 0;
     box-sizing: border-box;
+  }
+  html, body, #root {
+    height: 100%;
   }
   body {
     background: ${props => props.theme.colors.background};
+    -webkit-font-smoothing: antialiased;
     font-size: 14px;
     color: ${props => props.theme.colors.text};
-    font-family: sans-serif;
+  }
+  body, input, button {
+    font-family: Roboto, sans-serif;
   }
 `;
