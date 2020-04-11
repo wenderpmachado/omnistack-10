5e6b46b43db17270aafc83c0822fddd81676c45d
Wender Machado <wender.machado@rgsistemas.com.br>
2020-04-10 23:05:06 -0300

    :bug: Fixed getMany from NestJsx (not support mongo completly)

--- api/src/developers/developers.service.ts
+++ api/src/developers/developers.service.ts
@@ -3,11 +3,17 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { TypeOrmCrudService } from '@nestjsx/crud-typeorm';
 
 import { Developer } from './developer.entity';
+import { CrudRequest, GetManyDefaultResponse, Override } from '@nestjsx/crud';
 
 @Injectable()
 export class DevelopersService extends TypeOrmCrudService<Developer> {
   constructor(@InjectRepository(Developer) repo) {
     super(repo);
+  }
+
+  @Override()
+  getMany(req: CrudRequest): Promise<GetManyDefaultResponse<Developer> | Developer[]> {
+    return this.repo.find();
   }
 
   async create(developer: Developer) {
